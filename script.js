dfb7b35909c2d8e851d4e8546e3b25520621f8f3
catedony <ekaterina.dony@yandex.ru>
2018-10-04 18:47:03 +0300

    fixed some bugs and added a footer

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
 var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]; 
-window.onload = getData;
+
+window.onload = getContent;
+
 $('.tab').click(selectTab);
+
 $('.search').click(function() {
   $('.main').empty();
-  let user = $('.input').val();
+  	let user = $('.input').val();
    let urlChannels = 'https://wind-bow.gomix.me/twitch-api/channels/' + user + '?callback=?';
    let urlStreams = 'https://wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?';
 
-   $.getJSON(urlChannels, function(data) {
-      let name = data.display_name;
-      let logo = data.logo;
-      let url = data.url; 
-     if(data.name){
-        $('.main').append('<a href="'+ url + '" target="_blank" class="user ' + user + '"></a>');
-      $('.' + user).append('<div class="logo"><img width="200" src="' + logo +'"></div><div class="name">' + name + '</div>');
-     } else {
-       $('.main').html('Not found');
-     }
-     
-    });
-  
-  $.getJSON(urlStreams, function(data) {
-      if (data.stream) {
-        let status = data.stream.channel.status;
-        if (status.length > 30) {
-          status = status.slice(0, 30) + '...';
-        }
-       
-        $('.' + user).attr('title', data.stream.channel.status).addClass('online').append('<div class="status">' + status + '</div>');
-      } else {
-        $('.' + user).addClass('offline').append('<div class="status">Offline</div>');
-      }
-    });
-
+	 loadData(user, urlChannels, urlStreams);
 }) 
 
 function selectTab() {
@@ -53,33 +31,33 @@ function selectTab() {
   }
 }
 
-function getData() {
+function getContent() {
   $.each(users, function(i, user) {
-    let urlChannel = 'https://wind-bow.gomix.me/twitch-api/channels/' + user + '?callback=?';
-    
-    $.getJSON(urlChannel, function(data) {
-      let name = data.display_name;
-      let logo = data.logo;
-      let url = data.url;
-      $('.main').append('<a href="'+ url + '" target="_blank" class="user ' + user + '"></a>');
-      $('.' + user).append('<div class="logo"><img width="200" src="' + logo +'"></div><div class="name">' + name + '</div>');
-    });
+    let urlChannels = 'https://wind-bow.gomix.me/twitch-api/channels/' + user + '?callback=?';
+		let urlStreams = 'https://wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?'; 
+		loadData(user, urlChannels, urlStreams);
   });
- $.each(users, function(i, user) {
-   let urlStreams = 'https://wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?'; 
-   $.getJSON(urlStreams, function(data) {
-      if (data.stream) {
-        let status = data.stream.channel.status;
-        if (status.length > 30) {
-          status = status.slice(0, 30) + '...';
-        }
-        
-        $('.' + user).attr('title', data.stream.channel.status).addClass('online').append('<div class="status">' + status + '</div>');
-      } else {
-        $('.' + user).addClass('offline').append('<div class="status">Offline</div>');
-      }
-    });
-  });
- 
 }
 
+function loadData(user, urlChannels, urlStreams) {
+	$.getJSON(urlChannels, function(data) {
+		let name = data.display_name;
+		let logo = data.logo;
+		let url = data.url;
+		$('.main').append('<a href="'+ url + '" target="_blank" class="user ' + user + '"></a>');
+		$('.' + user).append('<div class="logo"><img width="200" src="' + logo +'"></div><div class="name">' + name + '</div>');
+	})
+	.then(function(){
+		$.getJSON(urlStreams, function(data) {
+			 if (data.stream) {
+				 let status = data.stream.channel.status;
+				 if (status.length > 30) {
+					 status = status.slice(0, 30) + '...';
+				 }
+				 $('.' + user).attr('title', data.stream.channel.status).addClass('online').append('<div class="status">' + status + '</div>');
+			 } else {
+				 $('.' + user).addClass('offline').append('<div class="status">Offline</div>');
+			 }
+		 });
+	})
+}
